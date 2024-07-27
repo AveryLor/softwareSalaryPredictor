9d2e275a8957c60168a7f1bf3ef767f2ed128912
Avery Lor <averywlor@gmail.com>
2024-07-27 13:42:39 -0400

    Fixing push and pull

---
+++ predict_page.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+def load_model(): 
+    with open('saved_steps.pkl', 'rb') as file: 
+        data = pickle.load(file)
+    return data
+
+
+data = load_model()
+
+regressor_loaded = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page(): 
+    st.title("Software Developer Salary Prediction")
+
+
+    st.write("""### We need some information to predict the salary""")
+
+    countries = (
+        "United States",
+        "India",
+        "United Kingdom",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden",
+    )
+
+    education = (
+        "Less than a Bachelors",
+        "Bachelor’s degree",
+        "Master’s degree",
+        "Post grad",
+    )
+
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education Level", education)
+    expericence = st.slider("Years of Experience", 0, 50, 3)
